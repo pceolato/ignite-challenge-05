827ab222054c4f0ac7b028af2725e250c4209275
Pedro Ceolato <pedroceolato2002@gmail.com>
2022-06-29 17:49:30 -0300

    refactor: page Home

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react';
 import { GetStaticProps } from 'next';
 import Link from 'next/link';
+import  Head  from 'next/head';
 import Header from '../components/Header';
 import { FiCalendar, FiUser } from 'react-icons/fi'
 
@@ -11,6 +12,7 @@ import { getPrismicClient } from '../services/prismic';
 
 import commonStyles from '../styles/common.module.scss';
 import styles from './home.module.scss';
+
 
 interface Post {
   uid?: string;
@@ -47,37 +49,79 @@ export default function Home({ postsPagination }: HomeProps) {
   })
 
   const [posts, setPosts] = useState<Post[]>(formattedPost)
+  const [nextPage, setNextPage] = useState(postsPagination.next_page)
+  const [currentPage, setCurrentPage] = useState(1)
+
+  async function handleNextPage(): Promise<void> {
+    if (currentPage !== 1 && nextPage === null) {
+      return;
+    }
+
+    const postsResults = await fetch(`${nextPage}`)
+      .then(response => response.json())
+
+    setNextPage(postsResults.next_page)
+    setCurrentPage(postsResults.page)
+
+    const newPosts = postsResults.results.map(post => {
+      return {
+        uid: post.uid,
+        first_publication_date: format(
+          new Date(post.first_publication_date),
+          'dd MMM yyyy',
+          {
+            locale: ptBR,
+          }
+        ),
+        data: {
+          title: post.data.title,
+          subtitle: post.data.subtitle,
+          author: post.data.author,
+        }
+      }
+    })
+
+    setPosts([...posts, ...newPosts])
+  }
 
   return (
-    <main className={commonStyles.container}>
-      <Header />
+    <>
+    <Head>
+      <title>Home | spacetraveling</title>
+    </Head>
 
-      <div className={styles.posts}>
-        {posts.map(post => (
-          <Link href={`/post/${post.uid}`} key={post.uid}>
-            <a className={styles.post}>
-              <strong>{post.data.title}</strong>
-              <p>{post.data.subtitle}</p>
-              <ul>
-                <li>
-                  <FiCalendar />
-                  {post.first_publication_date}
-                </li>
-                <li>
-                  <FiUser />
-                  {post.data.author}
-                </li>
-              </ul>
-            </a>
-          </Link>
-        ))}
+      <main className={commonStyles.container}>
+        <Header />
 
-        <button type="button">
-          Carregar mais posts
-        </button>
+        <div className={styles.posts}>
+          {posts.map(post => (
+            <Link href={`/post/${post.uid}`} key={post.uid}>
+              <a className={styles.post}>
+                <strong>{post.data.title}</strong>
+                <p>{post.data.subtitle}</p>
+                <ul>
+                  <li>
+                    <FiCalendar />
+                    {post.first_publication_date}
+                  </li>
+                  <li>
+                    <FiUser />
+                    {post.data.author}
+                  </li>
+                </ul>
+              </a>
+            </Link>
+          ))}
 
-      </div>
-    </main>
+          {nextPage && (
+            <button type="button" onClick={handleNextPage}>
+              Carregar mais posts
+            </button>
+          )}
+
+        </div>
+      </main>
+    </>
   )
 }
 
